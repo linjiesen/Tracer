a90448dc09adbe0274a458152f2f4eb60eb023fb
Alroy <347625023@qq.com>
2021-04-12 22:12:44 +0800

    统计页面完成

--- web/urls.py
+++ web/urls.py
@@ -51,7 +51,10 @@ urlpatterns = [
 
         url(r'^dashboard/$', dashboard.dashboard, name='dashboard'),
         url(r'^dashboard/issues/chart/$', dashboard.issues_chart, name='issues_chart'),
+
         url(r'^statistics/$', statistics.statistics, name='statistics'),
+        url(r'^statistics/priority/$', statistics.statistics_priority, name='statistics_priority'),
+        url(r'^statistics/project/user/$', statistics.statistics_project_user, name='statistics_project_user'),
 
     ], None)),
     url(r'^invite/join/(?P<code>\w+)$', issues.invite_join, name='invite_join'),
